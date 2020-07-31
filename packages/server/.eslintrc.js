16db705eab790c1d19b6765a83cb5816453cf1a1
João Victor <jv782063@gmail.com>
2020-07-30 21:35:38 -0300

    build(global): starts the project
    Starts the project

---
+++ packages/server/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    node: true,
+    es2020: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    camelcase: 'off'
+  }
+}
