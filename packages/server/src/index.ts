f3d480c179500b88f2ba6c182a80d333fa49f05e
João Victor <jv782063@gmail.com>
2020-08-02 13:18:23 -0300

    style(server): transfer schemas to a dedicated file
    Transfer schemas to a dedicated file

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,8 @@
 import { GraphQLServer } from 'graphql-yoga'
 import resolvers from './resolvers'
-import typeDefs from './schema'
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
